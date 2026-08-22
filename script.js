d4eea01534a73ced542066ee0958cf727834b1cd
thihathihaaung1111-cmyk <thihathihaaung1111@gmail.com>
2026-08-22 18:01:17 +0630

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// Ayeyarwady Weather
+const locationBtn = document.getElementById("locationBtn");
+const locationText = document.getElementById("location");
+
+locationBtn.addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    locationText.innerText = "ဒီ Browser မှာ Location မရပါ။";
+    return;
+  }
+
+  locationText.innerText = "တည်နေရာ ရှာနေသည်...";
+
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+
+      locationText.innerHTML = `
+      📍 Latitude : ${lat}<br>
+      📍 Longitude : ${lon}
+      `;
+    },
+    () => {
+      locationText.innerText = "တည်နေရာ ခွင့်ပြုချက် မရရှိပါ။";
+    }
+  );
+});
