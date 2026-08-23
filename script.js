4d3e385e7a0351451f3d34ec15ba30550f022148
thihathihaaung1111-cmyk <thihathihaaung1111@gmail.com>
2026-08-23 14:39:51 +0630

    Update script.js

--- script.js
+++ script.js
@@ -43,3 +43,43 @@ locationBtn.addEventListener("click", () => {
         document.getElementById("location").innerText = "❌ တည်နေရာ ရယူ၍ မရပါ။ Permission ပေးပါ။";
     });
 });
+
+
+function getDistance(lat1, lon1, lat2, lon2) {
+    const R = 6371;
+
+    const dLat = (lat2 - lat1) * Math.PI / 180;
+    const dLon = (lon2 - lon1) * Math.PI / 180;
+
+    const a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(lat1 * Math.PI / 180) *
+        Math.cos(lat2 * Math.PI / 180) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    return R * c;
+}
+
+function findNearestLocation(userLat, userLon, locations) {
+    let nearest = null;
+    let shortestDistance = Infinity;
+
+    for (const place of locations) {
+        const distance = getDistance(
+            userLat,
+            userLon,
+            place.latitude,
+            place.longitude
+        );
+
+        if (distance < shortestDistance) {
+            shortestDistance = distance;
+            nearest = place;
+        }
+    }
+
+    return nearest;
+}
