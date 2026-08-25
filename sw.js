e3d86b8d3ac4907ecc405c180eea4103115189f3
thihathihaaung1111-cmyk <thihathihaaung1111@gmail.com>
2026-08-25 19:50:44 +0630

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,71 @@
-self.addEventListener("fetch",(event)=>{
+const CACHE_NAME = "ayeyarwady-weather-v2";
 
-event.respondWith(
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./locations.json",
+  "./offline.html"
+];
 
-fetch(event.request)
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 
-.then((response)=>{
-
-const copy=response.clone();
-
-caches.open(CACHE_NAME)
-
-.then(cache=>cache.put(event.request,copy));
-
-return response;
-
-})
-
-.catch(()=>{
-
-return caches.match(event.request)
-
-.then(res=>{
-
-return res || caches.match("./offline.html");
-
+  self.skipWaiting();
 });
 
-})
+// Activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
 
-);
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+
+  event.respondWith(
+
+    fetch(event.request)
+
+      .then((response) => {
+
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      })
+
+      .catch(() => {
+
+        return caches.match(event.request).then((res) => {
+
+          return res || caches.match("./offline.html");
+
+        });
+
+      })
+
+  );
 
 });
